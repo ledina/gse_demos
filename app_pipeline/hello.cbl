576d6d5ed2a4c5001ebf510418a2387c48ce0ff9
Ledina Hido-Evans <ledina.hido@uk.ibm.com>
2024-06-06 21:54:49 +0100

    Install the resources

--- app_pipeline/hello.cbl
+++ app_pipeline/hello.cbl
@@ -3,10 +3,10 @@
         ENVIRONMENT DIVISION.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-        01 MSG  PIC X(16) VALUE "GSE UK!".
+        01 MSG  PIC X(16) VALUE "Hello GSE UK!".
         LINKAGE SECTION.
         PROCEDURE DIVISION.
-            DISPLAY "Hello and welcome to...".
+            DISPLAY "Hello".
             EXEC CICS SEND TEXT FROM (MSG)
                  END-EXEC
             EXEC CICS RETURN
