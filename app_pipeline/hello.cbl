abccc35c04ac61b69126b258e8abcdcbbcef6f2e
Ledina Hido-Evans <ledina.hido@uk.ibm.com>
2024-06-07 14:01:26 +0100

    back to uk

--- app_pipeline/hello.cbl
+++ app_pipeline/hello.cbl
@@ -3,7 +3,7 @@
         ENVIRONMENT DIVISION.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-        01 MSG  PIC X(18) VALUE "Hello GSE Nordic!".
+        01 MSG  PIC X(18) VALUE "Hello GSE UK!".
         LINKAGE SECTION.
         PROCEDURE DIVISION.
             DISPLAY "Hello".
